89cd6934900c9ee806cc1c673bc45900870e10e3
dev <brizotg@gmail.com>
2022-11-21 15:53:56 +0100

    Fix Data.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider {
      * @return void
      */
     public function register() {
-        
+        include_once base_path('config-propel/propel.config.php');
     }
 
 }
